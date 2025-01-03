9caa212ac97e9479cadd0285b2f65747d2bd3997
Ionut Pac-Balaj <ipacbalaj@gmail.com>
2025-01-03 13:01:07 +0200

    Disable sampling entirely

--- OrderManagement/OrderManagement/Samplers/PercentageSampler.cs
+++ OrderManagement/OrderManagement/Samplers/PercentageSampler.cs
@@ -7,7 +7,7 @@ public class PercentageSampler: Sampler
     public override SamplingResult ShouldSample(in SamplingParameters samplingParameters)
     {
         Console.WriteLine($"PercentageSampler invoked for activity: {samplingParameters.Name}, TraceId: {samplingParameters.TraceId}");
-        var result = new SamplingResult(true);
+        var result = new SamplingResult(false);
         Console.WriteLine($"Sampling Decision: {result.Decision}");
         return result;
     }
